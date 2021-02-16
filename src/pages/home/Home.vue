c4d7dced89633e137de063ad104ad04c69867ff9
BarneyWong <barneywong@icloud.com>
2021-02-16 10:16:58 +0800

    swiper

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,13 +1,18 @@
 <template>
+  <div>
     <Header></Header>
+    <home-swiper></home-swiper>
+  </div>
 </template>
 
 <script>
 import Header from './components/Header'
+import HomeSwiper from './components/HomeSwiper'
 export default {
   name: 'Home',
   components: {
-    Header: Header
+    Header: Header,
+    HomeSwiper: HomeSwiper
   }
 }
 </script>
---
+++ src/pages/home/components/HomeSwiper.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="wrapper">
+    <swiper  :options="swiperOptions">
+      <swiper-slide v-for="item of swiperList" v-bind:key="item.id">
+        <img class="swiper-img" v-bind:src="item.imgUrl">
+      </swiper-slide>
+      <div class="swiper-pagination" slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data: function () {
+    return {
+      swiperOptions: {
+        pagination: {
+          el: '.swiper-pagination'
+        },
+        loop: true
+      },
+      swiperList: [
+        {
+          id: '0001',
+          imgUrl: 'https://dimg04.c-ctrip.com/images/0zg2i120008a7304uC5C6.jpg'
+        },
+        {
+          id: '0002',
+          imgUrl: 'https://dimg04.c-ctrip.com/images/0zg2i120008a7304uC5C6.jpg'
+        }
+      ]
+    }
+  }
+}
+
+</script>
+
+<style lang="stylus" scoped>
+.wrapper >>> .swiper-pagination-bullet-active
+  background red !important
+
+.wrapper
+  overflow hidden
+  width 100%
+  height 0
+  padding-bottom 31.25%
+
+  .swiper-img
+    width 100%
+</style>

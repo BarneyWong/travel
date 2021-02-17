4fcaa5b9ed98e8bb2d7af165a09e00f955cf0f7e
BarneyWong <barneywong@icloud.com>
2021-02-17 22:57:11 +0800

    city alphabeet

--- src/pages/city/components/CityList.vue
+++ src/pages/city/components/CityList.vue
@@ -1,46 +1,87 @@
 <template>
-  <div class="list">
-    <div class="area">
-      <div class="title border-topbottom">current city</div>
-      <div class="button-list">
-        <div class="button-wrapper">
-          <div class="button">Xian</div>
+  <div class="list" ref="wrapper">
+    <div>
+      <div class="area">
+        <div class="title border-topbottom">current city</div>
+        <div class="button-list">
+          <div class="button-wrapper">
+            <div class="button">Xian</div>
+          </div>
         </div>
       </div>
-    </div>
-    <div class="area">
-      <div class="title border-topbottom">popular city</div>
-      <div class="button-list">
-        <div class="button-wrapper">
-          <div class="button">Xian</div>
-        </div>
-        <div class="button-wrapper">
-          <div class="button">Xian</div>
-        </div>
-        <div class="button-wrapper">
-          <div class="button">Xian</div>
-        </div>
-        <div class="button-wrapper">
-          <div class="button">Xian</div>
-        </div>
-        <div class="button-wrapper">
-          <div class="button">Xian</div>
+      <div class="area">
+        <div class="title border-topbottom">popular city</div>
+        <div class="button-list">
+          <div class="button-wrapper">
+            <div class="button">Xian</div>
+          </div>
+          <div class="button-wrapper">
+            <div class="button">Xian</div>
+          </div>
+          <div class="button-wrapper">
+            <div class="button">Xian</div>
+          </div>
+          <div class="button-wrapper">
+            <div class="button">Xian</div>
+          </div>
+          <div class="button-wrapper">
+            <div class="button">Xian</div>
+          </div>
         </div>
       </div>
-    </div>
-    <div class="area">
-      <div class="title border-topbottom">A</div>
-      <div class="item-list">
-        <div class="item border-bottom">test</div>
-        <div class="item border-bottom">test</div>
+      <div class="area">
+        <div class="title border-topbottom">A</div>
+        <div class="item-list">
+          <div class="item border-bottom">test</div>
+          <div class="item border-bottom">test</div>
+          <div class="item border-bottom">test</div>
+          <div class="item border-bottom">test</div>
+          <div class="item border-bottom">test</div>
+          <div class="item border-bottom">test</div>
+          <div class="item border-bottom">test</div>
+          <div class="item border-bottom">test</div>
+        </div>
+      </div>
+      <div class="area">
+        <div class="title border-topbottom">A</div>
+        <div class="item-list">
+          <div class="item border-bottom">test</div>
+          <div class="item border-bottom">test</div>
+          <div class="item border-bottom">test</div>
+          <div class="item border-bottom">test</div>
+          <div class="item border-bottom">test</div>
+          <div class="item border-bottom">test</div>
+          <div class="item border-bottom">test</div>
+          <div class="item border-bottom">test</div>
+        </div>
+      </div>
+      <div class="area">
+        <div class="title border-topbottom">A</div>
+        <div class="item-list">
+          <div class="item border-bottom">test</div>
+          <div class="item border-bottom">test</div>
+          <div class="item border-bottom">test</div>
+          <div class="item border-bottom">test</div>
+          <div class="item border-bottom">test</div>
+          <div class="item border-bottom">test</div>
+          <div class="item border-bottom">test</div>
+          <div class="item border-bottom">test</div>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import Bscroll from 'better-scroll'
 export default {
-  name: 'CityList'
+  name: 'CityList',
+  mounted () {
+    this.scroll = new Bscroll(this.$refs.wrapper, {
+      movable: true,
+      zoom: true
+    })
+  }
 }
 </script>
 
@@ -48,8 +89,10 @@ export default {
 .border-topbottom
   &:before
     border-color #ccc
+
   &:after
     border-color #ccc
+
 .list
   overflow hidden
   position absolute
@@ -57,11 +100,14 @@ export default {
   left 0
   right 0
   bottom 0
+
   .border-bottom
     &:before
       border-color #ccc
+
     &:after
       border-color #ccc
+
   .title
     line-height .44rem
     background #eee
@@ -83,6 +129,7 @@ export default {
         text-align center
         border .02rem solid #ccc
         border-radius .06rem
+
   .item-list
     .item
       line-height .76rem
